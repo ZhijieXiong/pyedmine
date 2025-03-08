86b56c8e986c315ff0c1f4a699df60a273a5c0bf
ZhijieXiong <18800118477@163.com>
2025-03-08 10:09:58 +0800

    update CD

--- examples/cognitive_diagnosis/train/hyper_cd_search_params.py
+++ examples/cognitive_diagnosis/train/hyper_cd_search_params.py
@@ -1,26 +1,23 @@
 import argparse
-
-from torch.utils.data import DataLoader
+from hyperopt import fmin, tpe, hp
 
 from set_params.congnitive_diagnosis_params import setup_common_args
 from config.hyper_cd import config_hyper_cd
+from utils import get_objective_func
 
 from edmine.utils.parse import str2bool
-from edmine.utils.use_torch import set_seed
-from edmine.dataset.CognitiveDiagnosisDataset import BasicCognitiveDiagnosisDataset
 from edmine.model.cognitive_diagnosis_model.HyperCD import HyperCD
-from edmine.trainer.DLCognitiveDiagnosisTrainer import DLCognitiveDiagnosisTrainer
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(parents=[setup_common_args()], add_help=False)
     # batch size
-    parser.add_argument("--train_batch_size", type=int, default=64)
-    parser.add_argument("--evaluate_batch_size", type=int, default=1024)
+    parser.add_argument("--train_batch_size", type=int, default=256)
+    parser.add_argument("--evaluate_batch_size", type=int, default=2048)
     # 优化器
     parser.add_argument("--optimizer_type", type=str, default="adam", choices=("adam", "sgd"))
-    parser.add_argument("--learning_rate", type=float, default=0.0001)
-    parser.add_argument("--weight_decay", type=float, default=0.0005)
+    parser.add_argument("--learning_rate", type=float, default=0.001)
+    parser.add_argument("--weight_decay", type=float, default=0.0001)
     parser.add_argument("--momentum", type=float, default=0.9)
     # scheduler配置
     parser.add_argument("--enable_scheduler", type=str2bool, default=False)
@@ -40,26 +37,30 @@ if __name__ == "__main__":
     parser.add_argument("--dim_feature", type=int, default=512)
     parser.add_argument("--dim_emb", type=int, default=16)
     parser.add_argument("--leaky", type=float, default=0.8)
-    # 其它
-    parser.add_argument("--save_model", type=str2bool, default=False)
-    parser.add_argument("--use_wandb", type=str2bool, default=False)
 
-    args = parser.parse_args()
-    params = vars(args)
-    set_seed(params["seed"])
-    global_params, global_objects = config_hyper_cd(params)
-
-    dataset_train = BasicCognitiveDiagnosisDataset(global_params["datasets_config"]["train"], global_objects)
-    dataloader_train = DataLoader(dataset_train, batch_size=params["train_batch_size"], shuffle=True)
-    dataset_valid = BasicCognitiveDiagnosisDataset(global_params["datasets_config"]["valid"], global_objects)
-    dataloader_valid = DataLoader(dataset_valid, batch_size=params["train_batch_size"], shuffle=False)
-
-    global_objects["data_loaders"] = {
-        "train_loader": dataloader_train,
-        "valid_loader": dataloader_valid
+    # 设置参数空间
+    parameters_space = {
+        "num_layer": [3, 4],
+        "dim_feature": [512, 1024],
+        "dim_emb": [8, 16],
     }
-    global_objects["models"] = {
-        "HyperCD": HyperCD(global_params, global_objects).to(global_params["device"])
+    space = {
+        param_name: hp.choice(param_name, param_space)
+        for param_name, param_space in parameters_space.items()
     }
-    trainer = DLCognitiveDiagnosisTrainer(global_params, global_objects)
-    trainer.train()
+    num = 1
+    for parameter_space in parameters_space.values():
+        num *= len(parameter_space)
+    if num > 100:
+        max_evals = 20 + int(num * 0.2)
+    elif num > 50:
+        max_evals = 15 + int(num * 0.2)
+    elif num > 20:
+        max_evals = 10 + int(num * 0.2)
+    elif num > 10:
+        max_evals = 5 + int(num * 0.2)
+    else:
+        max_evals = num
+    current_best_performance = 0
+    fmin(get_objective_func(parser, config_hyper_cd, "HyperCD", HyperCD), space, algo=tpe.suggest, max_evals=max_evals)
+
