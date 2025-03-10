36b9cce0f294b269666129b6ad42cca0e35b1f39
ZhijieXiong <18800118477@163.com>
2025-03-10 21:13:14 +0800

    Optimized evaluator code

--- edmine/evaluator/SequentialDLKTEvaluator.py
+++ edmine/evaluator/SequentialDLKTEvaluator.py
@@ -12,61 +12,89 @@ class SequentialDLKTEvaluator(DLEvaluator):
         super().__init__(params, objects)
 
     def inference(self, model, data_loader):
+        evaluate_overall = self.params["sequential_dlkt"]["evaluate_overall"]
         seq_start = self.params["sequential_dlkt"]["seq_start"]
-        cold_start = self.params["sequential_dlkt"]["cold_start"]
+        question_cold_start = self.params["sequential_dlkt"]["question_cold_start"]
+        user_cold_start = self.params["sequential_dlkt"]["user_cold_start"]
         multi_step = self.params["sequential_dlkt"]["multi_step"]
+        use_core = self.params["sequential_dlkt"]["use_core"]
 
         predict_score_all = []
         ground_truth_all = []
+        question_id_all = []
         question_all = []
-        # concept_all = []
         # result_all_batch是batch格式，即(num_batch * batch_size, seq_len)
         result_all_batch = []
-        for batch in tqdm(data_loader, desc="one step inference"):
-            correctness_seq = batch["correctness_seq"]
-            mask_bool_seq = torch.ne(batch["mask_seq"], 0)
-            predict_score = model.get_predict_score(batch, seq_start)["predict_score"].detach().cpu().numpy()
-            ground_truth = torch.masked_select(correctness_seq[:, seq_start-1:], mask_bool_seq[:, seq_start-1:]).detach().cpu().numpy()
-            predict_score_all.append(predict_score)
-            ground_truth_all.append(ground_truth)
+        inference_result = {}
+        if evaluate_overall or (question_cold_start >= 0) or (user_cold_start >= 1):
+            for batch in tqdm(data_loader, desc="one step inference"):
+                correctness_seq = batch["correctness_seq"]
+                mask_seq = torch.ne(batch["mask_seq"], 0)
+                question_seq = batch["question_seq"]
+                predict_score = model.get_predict_score(batch, seq_start)["predict_score"].detach().cpu().numpy()
+                ground_truth = torch.masked_select(correctness_seq[:, seq_start-1:], mask_seq[:, seq_start-1:]).detach().cpu().numpy()
+                question_id = torch.masked_select(question_seq[:, seq_start-1:], mask_seq[:, seq_start-1:]).detach().cpu().numpy()
+                predict_score_all.append(predict_score)
+                ground_truth_all.append(ground_truth)
+                question_id_all.append(question_id)
 
-            # 冷启动计算
-            question_seq = batch["question_seq"]
-            predict_score_batch = model.get_predict_score(batch)["predict_score_batch"]
-            result_all_batch.append({
-                "question": question_seq[:, 1:].detach().cpu().numpy(),
-                "label": correctness_seq[:, 1:].detach().cpu().numpy(),
-                "predict_score": predict_score_batch.detach().cpu().numpy(),
-                "mask": batch["mask_seq"][:, 1:].detach().cpu().numpy()
-            })
+                # 冷启动计算
+                question_seq = batch["question_seq"]
+                predict_score_batch = model.get_predict_score(batch)["predict_score_batch"]
+                result_all_batch.append({
+                    "question": question_seq[:, 1:].detach().cpu().numpy(),
+                    "label": correctness_seq[:, 1:].detach().cpu().numpy(),
+                    "predict_score": predict_score_batch.detach().cpu().numpy(),
+                    "mask": batch["mask_seq"][:, 1:].detach().cpu().numpy()
+                })
 
-            # core指标计算
-            question_all.append(torch.masked_select(question_seq[:, 1:], mask_bool_seq[:, 1:]).detach().cpu().numpy())
+                # core指标计算
+                question_all.append(torch.masked_select(question_seq[:, 1:], mask_seq[:, 1:]).detach().cpu().numpy())
 
-        predict_score_all = np.concatenate(predict_score_all, axis=0)
-        ground_truth_all = np.concatenate(ground_truth_all, axis=0)
-        inference_result = get_kt_metric(ground_truth_all, predict_score_all)
+            predict_score_all = np.concatenate(predict_score_all, axis=0)
+            ground_truth_all = np.concatenate(ground_truth_all, axis=0)
+            inference_result.update(get_kt_metric(ground_truth_all, predict_score_all))
 
-        inference_result["core"] = {
-            "repeated": core_metric(predict_score_all, ground_truth_all, np.concatenate(question_all, axis=0), True),
-            "non-repeated": core_metric(predict_score_all, ground_truth_all, np.concatenate(question_all, axis=0), False)
-        }
+        if use_core:
+            inference_result["core"] = {
+                "repeated": core_metric(predict_score_all, ground_truth_all, np.concatenate(question_all, axis=0), True),
+                "non-repeated": core_metric(predict_score_all, ground_truth_all, np.concatenate(question_all, axis=0), False)
+            }
 
-        if cold_start >= 1:
-            predict_score_cold_start = []
-            ground_truth_cold_start = []
+        if user_cold_start >= 1:
+            predict_score_cold_start_u = []
+            ground_truth_cold_start_u = []
             for batch_result in result_all_batch:
                 batch_size = batch_result["mask"].shape[0]
                 seq_len = batch_result["mask"].shape[1]
                 cold_start_mask = np.ones((batch_size, seq_len))
-                cold_start_mask[:, cold_start:] = 0
+                cold_start_mask[:, user_cold_start:] = 0
                 mask = np.logical_and(cold_start_mask, batch_result["mask"])
-                predict_score_cold_start.append(batch_result["predict_score"][mask])
-                ground_truth_cold_start.append(batch_result["label"][mask])
-            predict_score_cold_start = np.concatenate(predict_score_cold_start, axis=0)
-            ground_truth_cold_start = np.concatenate(ground_truth_cold_start, axis=0)
-            inference_result["cold_start"] = get_kt_metric(ground_truth_cold_start, predict_score_cold_start)
+                predict_score_cold_start_u.append(batch_result["predict_score"][mask])
+                ground_truth_cold_start_u.append(batch_result["label"][mask])
+            predict_score_cold_start_u = np.concatenate(predict_score_cold_start_u, axis=0)
+            ground_truth_cold_start_u = np.concatenate(ground_truth_cold_start_u, axis=0)
+            inference_result["user_cold_start"] = get_kt_metric(ground_truth_cold_start_u, predict_score_cold_start_u)
 
+        if question_cold_start >= 0:
+            predict_score_cold_start_q = []
+            ground_truth_cold_start_q = []
+            cold_start_question = self.objects["cold_start_question"]
+            print("calculating question cold start metric ...")
+            for q_id, ps, gt in zip(question_id, predict_score, ground_truth):
+                if q_id in cold_start_question:
+                    predict_score_cold_start_q.append(ps)
+                    ground_truth_cold_start_q.append(gt)
+            if len(predict_score_cold_start_q) > 0:
+                inference_result["question_cold_start"] = get_kt_metric(ground_truth_cold_start_q, predict_score_cold_start_q)
+            else:
+                inference_result["question_cold_start"] = {
+                    "AUC": -1.,
+                    "ACC": -1.,
+                    "RMSE": -1.,
+                    "MAE": -1.
+                }
+        
         if multi_step > 1:
             inference_result["multi_step"] = {
                 "non-accumulate": self.multi_step_inference(model, data_loader, False),
@@ -110,34 +138,46 @@ class SequentialDLKTEvaluator(DLEvaluator):
         return get_kt_metric(ground_truth_all, predict_score_all)
 
     def log_inference_results(self):
+        evaluate_overall = self.params["sequential_dlkt"]["evaluate_overall"]
         seq_start = self.params["sequential_dlkt"]["seq_start"]
-        cold_start = self.params["sequential_dlkt"]["cold_start"]
+        question_cold_start = self.params["sequential_dlkt"]["question_cold_start"]
+        user_cold_start = self.params["sequential_dlkt"]["user_cold_start"]
         multi_step = self.params["sequential_dlkt"]["multi_step"]
+        use_core = self.params["sequential_dlkt"]["use_core"]
 
         for data_loader_name, inference_result in self.inference_results.items():
-            self.objects["logger"].info(f"evaluate result of {data_loader_name}")
-            performance = inference_result
-            self.objects["logger"].info(
-                f"    overall performances (seq_start {seq_start}) are AUC: "
-                f"{performance['AUC']:<9.5}, ACC: {performance['ACC']:<9.5}, "
-                f"RMSE: {performance['RMSE']:<9.5}, MAE: {performance['MAE']:<9.5}, ")
-
-            performance = inference_result["core"]["repeated"]
-            self.objects["logger"].info(
-                f"    core performances (seq_start {seq_start}, repeated) are AUC: "
-                f"{performance['AUC']:<9.5}, ACC: {performance['ACC']:<9.5}, "
-                f"RMSE: {performance['RMSE']:<9.5}, MAE: {performance['MAE']:<9.5}, ")
-
-            performance = inference_result["core"]["non-repeated"]
-            self.objects["logger"].info(
-                f"    core performances (seq_start {seq_start}, non-repeated) are AUC: "
-                f"{performance['AUC']:<9.5}, ACC: {performance['ACC']:<9.5}, "
-                f"RMSE: {performance['RMSE']:<9.5}, MAE: {performance['MAE']:<9.5}, ")
-
-            if cold_start >= 1:
-                performance = inference_result["cold_start"]
+            if evaluate_overall:
+                self.objects["logger"].info(f"evaluate result of {data_loader_name}")
+                performance = inference_result
                 self.objects["logger"].info(
-                    f"    cold start performances (cold_start is {cold_start}) are AUC: "
+                    f"    overall performances (seq_start {seq_start}) are AUC: "
+                    f"{performance['AUC']:<9.5}, ACC: {performance['ACC']:<9.5}, "
+                    f"RMSE: {performance['RMSE']:<9.5}, MAE: {performance['MAE']:<9.5}, ")
+
+            if use_core:
+                performance = inference_result["core"]["repeated"]
+                self.objects["logger"].info(
+                    f"    core performances (seq_start {seq_start}, repeated) are AUC: "
+                    f"{performance['AUC']:<9.5}, ACC: {performance['ACC']:<9.5}, "
+                    f"RMSE: {performance['RMSE']:<9.5}, MAE: {performance['MAE']:<9.5}, ")
+
+                performance = inference_result["core"]["non-repeated"]
+                self.objects["logger"].info(
+                    f"    core performances (seq_start {seq_start}, non-repeated) are AUC: "
+                    f"{performance['AUC']:<9.5}, ACC: {performance['ACC']:<9.5}, "
+                    f"RMSE: {performance['RMSE']:<9.5}, MAE: {performance['MAE']:<9.5}, ")
+
+            if user_cold_start >= 1:
+                performance = inference_result["user_cold_start"]
+                self.objects["logger"].info(
+                    f"    user cold start performances (cold_start is {user_cold_start}) are AUC: "
+                    f"{performance['AUC']:<9.5}, ACC: {performance['ACC']:<9.5}, "
+                    f"RMSE: {performance['RMSE']:<9.5}, MAE: {performance['MAE']:<9.5}, ")
+                
+            if question_cold_start >= 0:
+                performance = inference_result["question_cold_start"]
+                self.objects["logger"].info(
+                    f"    question cold start performances (cold_start is {question_cold_start}) are AUC: "
                     f"{performance['AUC']:<9.5}, ACC: {performance['ACC']:<9.5}, "
                     f"RMSE: {performance['RMSE']:<9.5}, MAE: {performance['MAE']:<9.5}, ")
 
--- examples/knowledge_tracing/evaluate/config.py
+++ examples/knowledge_tracing/evaluate/config.py
@@ -1,6 +1,9 @@
 import json
 import os
 import inspect
+from collections import defaultdict
+
+from utils import *
 
 from edmine.data.FileManager import FileManager
 from edmine.config.data import config_q_table
@@ -9,6 +12,7 @@ from edmine.config.model import config_general_dl_model
 from edmine.model.load_model import load_dl_model
 from edmine.utils.check import check_kt_seq_start
 from edmine.utils.log import get_now_time
+from edmine.utils.data_io import read_kt_file, read_json, write_json
 
 
 current_file_name = inspect.getfile(inspect.currentframe())
@@ -30,17 +34,44 @@ def config_sequential_dlkt(local_params):
         log_path = None
     config_logger(local_params, global_objects, log_path)
     config_general_dl_model(local_params, global_params)
-
-    check_kt_seq_start(local_params["seq_start"])
+    check_kt_seq_start(local_params.get("seq_start", 2))
     global_params["sequential_dlkt"] = {
-        "seq_start": local_params["seq_start"],
-        "cold_start": local_params["cold_start"],
-        "multi_step": local_params["multi_step"]
+        "seq_start": local_params.get("seq_start", 2),
+        "question_cold_start": local_params.get("question_cold_start", -1),
+        "user_cold_start": local_params.get("user_cold_start", 0),
+        "multi_step": local_params.get("multi_step", 1),
+        "use_core": local_params.get("use_core", False),
+        "evaluate_overall": local_params.get("evaluate_overall", True)
     }
-
     config_q_table(local_params, global_params, global_objects)
-    model_name = local_params["model_dir_name"]
-    model_dir = os.path.join(MODEL_DIR, model_name)
+    
+    model_name, setting_name, train_file_name = get_model_info(local_params["model_dir_name"])
+    setting_dir = global_objects["file_manager"].get_setting_dir(setting_name)
+    cold_start_dir = os.path.join(setting_dir, "data4cold_start")
+    if not os.path.exists(cold_start_dir):
+        os.mkdir(cold_start_dir)
+    question_cold_start = global_params["sequential_dlkt"]["question_cold_start"]
+    if question_cold_start >= 0:
+        cold_start_question_path = os.path.join(cold_start_dir, f"cold_start_question_{question_cold_start}.json")
+        if os.path.exists(cold_start_question_path):
+            global_objects["cold_start_question"] = read_json(cold_start_question_path)
+        else:
+            train_file_path = os.path.join(setting_dir, train_file_name + ".txt")
+            train_data = read_kt_file(train_file_path)
+            num_q_in_train = defaultdict(int)
+            for item_data in train_data:
+                seq_len = item_data["seq_len"]
+                question_seq = item_data["question_seq"][:seq_len]
+                for question_id in question_seq:
+                    num_q_in_train[question_id] += 1
+            global_objects["num_q_in_train"] = num_q_in_train
+            global_objects["cold_start_question"] = []
+            for question_id, num_question in num_q_in_train.items():
+                if num_question <= local_params["question_cold_start"]:
+                    global_objects["cold_start_question"].append(question_id)
+            write_json(global_objects["cold_start_question"], cold_start_question_path)
+            
+    model_dir = os.path.join(MODEL_DIR, local_params["model_dir_name"])
     model = load_dl_model(global_params, global_objects,
                           model_dir, local_params["model_name"], local_params["model_name_in_ckt"])
     global_params["evaluator_config"] = {"model_name": model_name}
