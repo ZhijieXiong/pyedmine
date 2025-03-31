929eff65265075e5a7ba54edc73e40d1d0c38481
xzj <laboratory2022@126.com>
2025-03-31 10:02:09 +0800

    update atdkt search params

--- examples/knowledge_tracing/train/atdkt_search_params.py
+++ examples/knowledge_tracing/train/atdkt_search_params.py
@@ -21,19 +21,20 @@ if __name__ == "__main__":
     parser.add_argument("--momentum", type=float, default=0.9)
     # 模型参数
     parser.add_argument("--dim_emb", type=int, default=64)
-    parser.add_argument("--dim_latent", type=int, default=64)
+    parser.add_argument("--dim_latent", type=int, default=256)
     parser.add_argument("--num_rnn_layer", type=int, default=1)
     parser.add_argument("--dropout", type=float, default=0.2)
-    parser.add_argument("--IK_start", type=int, default=20)
+    parser.add_argument("--IK_start", type=int, default=30)
     parser.add_argument("--w_QT_loss", type=float, default=1)
     parser.add_argument("--w_IK_loss", type=float, default=1)
 
     # 设置参数空间
     parameters_space = {
         "weight_decay": [0.0001, 0.00001, 0],
+        "dim_emb": [64, 256],
         "dropout": [0.1, 0.2, 0.3],
-        "w_QT_loss": [0.1, 0.2, 0.5, 1],
-        "w_IK_loss": [0.1, 0.2, 0.5, 1],
+        "w_QT_loss": [0.1, 0.5, 1],
+        "w_IK_loss": [0.1, 0.5, 1],
     }
     space = {
         param_name: hp.choice(param_name, param_space)
